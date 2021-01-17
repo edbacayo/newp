24c0227adf58467d83f82e34f41e09152dabfb1f
Ed Bacayo <edbacayo@yahoo.com>
2021-01-18 05:59:07 +0800

    Update API call

--- public/js/faction.js
+++ public/js/faction.js
@@ -19,63 +19,73 @@ $.getJSON(tickURL, function (res) {
     var curr = new Date();
     var currHour = curr.getUTCHours();
     var currMinute = curr.getUTCMinutes();    
+    var tickInfo = "<h6 class=\"sub\">" + "Last Tick (updated): " + lastTick + " | Current: " + currHour + ":" + currMinute + "</h6>";
 
-    $("#faction-presence").after("<h6 class=\"sub\">" + "Last Tick (updated): " + lastTick + " | Current: " + currHour + ":" + currMinute + "</h6>")
+    // $("#faction-presence").after("<h6 class=\"sub\">" + "Last Tick (updated): " + lastTick + " | Current: " + currHour + ":" + currMinute + "</h6>")
+
+    getFactionInfo();
+    displayTickInfo(tickInfo);
+
 })
 
+function displayTickInfo(tickInfo) {
+    $("#faction-presence").after(tickInfo);
+}
 
-$.getJSON(facURL, function (res) {
-    var ed = res.docs[0].faction_presence;
-
-    if (ed.length > 0) {
-        var systemName = "";
-        var influence = 0;
-        var active_states = "";
-        var updatedAt = new Date();
-        var afterTick = "No";
-        var daysWon = "";
-        var opponent = "";
-        var conflict = "";
-
-        for (var i = 0; i < ed.length; i++) {
-            systemName = ed[i].system_name;
-            influence = (ed[i].influence * 100).toFixed(1);
-            if (ed[i].active_states.length > 0) {
-                active_states = "";
-                if (ed[i].active_states.length > 1) {
-                    for (var x = 0; x < ed[i].active_states.length; x++) {
-                        active_states += ed[i].active_states[x].state + " ";
+function getFactionInfo() {
+    $.getJSON(facURL, function (res) {
+        var ed = res.docs[0].faction_presence;
+    
+        if (ed.length > 0) {
+            var systemName = "";
+            var influence = 0;
+            var active_states = "";
+            var updatedAt = new Date();
+            var afterTick = "No";
+            var daysWon = "";
+            var opponent = "";
+            var conflict = "";
+    
+            for (var i = 0; i < ed.length; i++) {
+                systemName = ed[i].system_name;
+                influence = (ed[i].influence * 100).toFixed(1);
+                if (ed[i].active_states.length > 0) {
+                    active_states = "";
+                    if (ed[i].active_states.length > 1) {
+                        for (var x = 0; x < ed[i].active_states.length; x++) {
+                            active_states += ed[i].active_states[x].state + " ";
+                        }
+                    } else {
+                        active_states = ed[i].active_states[0].state;
                     }
                 } else {
-                    active_states = ed[i].active_states[0].state;
+                    active_states = "none";
                 }
-            } else {
-                active_states = "none";
+    
+                if (ed[i].conflicts.length > 0) {
+                    opponent = ed[i].conflicts[0].opponent_name;
+                    daysWon = ed[i].conflicts[0].days_won;
+                    conflict = opponent + "(Won: " + daysWon + ")";
+                } else {
+                    conflict = "NA";
+                }
+    
+                updatedAt = ed[i].updated_at;
+                if (Date.parse(updatedAt) > lastTickMillis) {
+                    afterTick = "Yes";
+                } else {
+                    afterTick = "No";
+                }
+    
+                $("#faction-presence").append("<tr><th scope=\"row\">" + systemName + "</th>" +
+                    "<td>" + influence + "</td>" +
+                    "<td>" + active_states + "</td>" +
+                    "<td>" + conflict + "</td>" +
+                    "<td>" + updatedAt + "(" + afterTick + ")" + "</td></tr>");
             }
-
-            if (ed[i].conflicts.length > 0) {
-                opponent = ed[i].conflicts[0].opponent_name;
-                daysWon = ed[i].conflicts[0].days_won;
-                conflict = opponent + "(Won: " + daysWon + ")";
-            } else {
-                conflict = "NA";
-            }
-
-            updatedAt = ed[i].updated_at;
-            if (Date.parse(updatedAt) > lastTickMillis) {
-                afterTick = "Yes";
-            } else {
-                afterTick = "No";
-            }
-
-
-
-            $("#faction-presence").append("<tr><th scope=\"row\">" + systemName + "</th>" +
-                "<td>" + influence + "</td>" +
-                "<td>" + active_states + "</td>" +
-                "<td>" + conflict + "</td>" +
-                "<td>" + updatedAt + "(" + afterTick + ")" + "</td></tr>");
         }
-    }
-})
+    })
+    
+    
+}
 
