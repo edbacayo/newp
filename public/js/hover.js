42a18050348cd8f2d2b3ed445a4b81d2e6ce5d4c
Ed Bacayo <edbacayo@yahoo.com>
2021-01-23 19:13:01 +0800

    Add card hover

--- public/js/hover.js
+++ public/js/hover.js
@@ -1,7 +1,7 @@
 $( ".card" ).hover(
     function() {
-      $(this).removeClass("shadow").css("cursor", "pointer"); 
+      $(this).removeClass("shadow").addClass("m-1"); 
     }, function() {
-      $(this).addClass("shadow");
+      $(this).addClass("shadow").removeClass("m-1");
     }
   );
